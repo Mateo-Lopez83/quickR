c41ce83ee86d06b4519c90380c8f7e30e26a7b78
MATEO_LOPEZ <m.lopez112@uniandes.edu.co>
2026-09-03 20:56:44 -0500

    primer commit ahora si

---
+++ transprot/src/header.rs
@@ -0,0 +1,20 @@
+pub struct Header{
+
+}
+
+pub struct ControlHeader{
+
+}
+
+
+impl Header{
+    fn marshallize(&self){
+        let b0:u8;
+    }
+}
+
+impl ControlHeader{
+    fn marshallize(&self){
+        let b0:u8;
+    }
+}
